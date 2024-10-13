3a1500643ec9400fcfe35a98f59e76846c0b0583
electronicsstudio <166483864+electronicsstudio@users.noreply.github.com>
2024-10-13 13:19:22 +0530

    Add files via upload

---
+++ DF_Mini_only.ino
@@ -0,0 +1,30 @@
+#include "DFRobotDFPlayerMini.h"
+#include <HardwareSerial.h>
+
+// Use the hardware serial ports on the ESP32
+
+DFRobotDFPlayerMini player;
+HardwareSerial mySerial(2); // Initialize hardware serial port 2
+
+void setup() 
+{
+  Serial.begin(115200);//
+  mySerial.begin(9600, SERIAL_8N1, 16, 17); // RX = 16, TX = 17
+
+  Serial.println("Initializing DFPlayer...");
+
+  if (!player.begin(mySerial)) {
+      Serial.println("Unable to begin:");
+      Serial.println("1. Please recheck the connection!");
+      Serial.println("2. Please insert the SD card!");
+      while (true);
+  }
+  Serial.println("DFPlayer Mini online.");
+
+  player.volume(10);  // Set volume value (0~30)
+  player.play(1);     // Play the first MP3 file
+}
+
+void loop() {
+    // Nothing to do here
+}
